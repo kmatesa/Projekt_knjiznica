7b59dd5e5fa414c8a0f55ed8b10b943c576d5333
kmatesa <kety.matesa@gmail.com>
2024-11-21 13:01:38 +0100

    Update indeks.js

--- backend/indeks.js
+++ backend/indeks.js
@@ -1,1 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const app = express();
+const port = 3000;
+
+// Parser za JSON podatke
+app.use(bodyParser.json());
+
+// Parser za podatke iz formi
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const connection = mysql.createConnection({
+    host: host
+    user: user,
+    password: password,
+    database: database
+  });
+  
+app.use(express.urlencoded({ extended: true }));
+  
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  app.listen(port, () => {
+    console.log("Server running at port: " + port);
+});
 
